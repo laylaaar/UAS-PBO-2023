5f1d086fbd248951f4b097d0cf34c7f02666dac7
laylaaar <62585431+laylaaar@users.noreply.github.com>
2023-01-16 23:54:23 +0700

    Add files via upload

---
+++ uas_nm1.py
@@ -0,0 +1,22 @@
+class animal:
+    def _init_(self,nama,spesies,ordo,kelas):
+        self.nama = nama
+        self.spesies = spesies
+        self.ordo = ordo
+        self.kelas = kelas
+
+    def show (self):
+        print("{}\n\t Spesies : {}\n\t Ordo   : {}\n\t Classification    : {}". format(self.nama,self.spesies,self.ordo,self.kelas))
+
+class animal_cat(animal):
+      def __init__(self,nama,spesies,ordo,kelas):
+        super()._init_(nama,spesies,ordo,kelas)
+        super().show()
+
+class animal_swan(animal):
+    def __init__(self,nama,spesies,ordo,kelas):
+        super()._init_(nama,spesies,ordo,kelas)
+        super().show()
+
+anggrek = animal_cat('Kucing','Felis Catus','Carnivora', 'Mamamlia')
+mawar = animal_swan('Angsa','Cygnini','Anseriformes','Aves')
